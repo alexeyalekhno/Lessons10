04c5e2f4657d7edcf08074f7b0f956ce3b62ab11
mrale <mrale@DESKTOP-HHS76V9>
2024-11-26 23:10:03 +0300

    Добавьте файлы проекта.

---
+++ Task1/Corner.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Corner
+    {
+        double gradus;
+        double min;
+        double sec;
+
+        public double Gradus
+        {
+            get
+            { 
+            return gradus;
+            }
+            set
+            {
+                if (value < 0 || value > 360)
+                {
+                    Console.WriteLine("Введено неверное значение");
+                }
+                else
+                { 
+                gradus = value;
+                }
+            }
+
+        }
+
+        public double Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    Console.WriteLine("Введено неверное значение");
+                }
+                else
+                {
+                    min = value;
+                }
+            }
+              
+              
+        }
+        public double Sec
+        {
+            get
+            { 
+            return sec;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    Console.WriteLine("Введено неверное значение");
+                }
+                else
+                {
+                    sec = value;
+                }
+            }
+        }
+
+        public Corner (double gradus, double min, double sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+           return (gradus + (min + sec / 60) / 60)*Math.PI/180;  
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные:");
+            double gradusProject = Convert.ToInt32(Console.ReadLine());
+            double minProject = Convert.ToInt32(Console.ReadLine());
+            double secProject = Convert.ToInt32(Console.ReadLine());
+     
+            Corner corner = new Corner(gradusProject,minProject,secProject);
+
+            double radians = corner.ToRadians();
+            Console.WriteLine(radians);
+            Console.ReadKey();
+        }
+    }
+}
